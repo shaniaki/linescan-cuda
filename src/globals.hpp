fd494451823141c78ade6bfeeb1f9e9af579769b
shaniaki <shaniaki@gmail.com>
2013-02-04 22:14:01 +0100

    added the 4th dataset with 510 nozzles

--- src/globals.hpp
+++ src/globals.hpp
@@ -8,14 +8,18 @@
 #ifndef GLOBALS_HPP_
 #define GLOBALS_HPP_
 
+#define NUM_RUNS 100
+
 // run which version of the parallel cuda kernel //
-#define KERNELVER compute_v1
+//#define KERNELVER compute_v1
 //#define KERNELVER compute_v2
-//#define KERNELVER compute_v3
+#define KERNELVER compute_v3
 
 // choose the input data set //
-//#define DATASET 0
-#define DATASET 1
+#define DATASET 0
+//#define DATASET 1
+//#define DATASET 3
+//#define DATASET 4
 
 #if DATASET==0
 	#define WHITE_VALUE 255
@@ -25,6 +29,10 @@
 	#define WHITE_VALUE 255
 	#define BUFFER_SIZE 24
 	#define N 16
+#elif DATASET==3 || DATASET==4
+	#define WHITE_VALUE 255
+	#define BUFFER_SIZE 24
+	#define N 510
 #endif
 
 struct aoi
